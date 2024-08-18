7f2f69874d0af42fe8ff4dd7317d32bd05da266e
Иван Колганов <110064602+IMKolganov@users.noreply.github.com>
2024-08-19 02:09:09 +0300

    init

---
+++ run.py
@@ -0,0 +1,44 @@
+# run.py
+
+from flask import Flask, Response, send_from_directory
+from flask_socketio import SocketIO, emit
+import cv2
+import os
+
+app = Flask(__name__, static_folder='public')
+socketio = SocketIO(app)
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            # Кодирование фрейма в JPEG
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            
+            # Отправка фрейма через WebSocket
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print("Client connected")
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("Client disconnected")
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5003)
